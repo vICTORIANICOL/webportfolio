73ba4c6ace3bad89ffd673167fb24845d68a6387
vIctorianicol <victorianicolenco04@gmail.com>
2025-08-14 09:49:23 +0200

    new projects

--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -3,6 +3,7 @@ import styles from "./projects.module.css";
 import img1 from "../../../assets/darkmode.png";
 import img3 from "../../../assets/Govalaimg.jpg";
 import img2 from "../../../assets/vagabond.png";
+import img4 from "../../../assets/skagenimg.png";
 
 export const Projects = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -35,6 +36,15 @@ export const Projects = () => {
         "This project is a mobile-only shopping application where users can browse and select products with ease. I implemented a full cart functionality, allowing users to add items, adjust quantities, remove products, and place orders directly from the app. The interface is optimized for smaller screens, ensuring a smooth and intuitive user experience from product selection to checkout.Additionally, the site includes a subscription option for users.",
       tech: ["HTML", "CSS", "React", "JavaScript", "Figma", "MongoDB"],
       link: "https://victorianicol.github.io/mcd_web_gowala_server/",
+    },
+
+    {
+      title: "Hotel",
+      image: img4,
+      description:
+        "During my internship at Indexed.dk, I worked from a provided Figma file to replicate the design and some functionality of the Hotels Skagen website using WordPress, Elementor, JavaScript and custom CSS. The site is fully responsive, optimized for both desktop and mobile. This is a training project and not the actual Hotels Skagen website.",
+      tech: ["Wordpress", "Elementor", "JavaScript", "CSS", "Figma"],
+      link: "",
     },
   ];
 
